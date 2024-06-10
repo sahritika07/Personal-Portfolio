cba768a40186c6766b0a4f913237b766892d2bed
sahritika07 <ritikasah731@gmail.com>
2024-06-11 00:26:54 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed =new Typed (".text", {
+    strings: [" I'm a Frontend Developer " , "And a UI/UX Developer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
